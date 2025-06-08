1cfdddd701d3340e8cc66002fdf0fc8b77c584d9
shigeki KOITO <skoito@LAPTOP-OH3M1IMB.>
2025-06-08 09:40:22 +0900

    add main.tf with S3 and cloudfront setup

--- main.tf
+++ main.tf
@@ -15,3 +15,53 @@ resource "aws_s3_bucket" "wiz_bucket" {
 resource "random_id" "rand" {
   byte_length = 4
 }
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "wiz-oac"
+  description                       = "OAC for S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "wiz_cf" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.wiz_bucket.bucket_regional_domain_name
+    origin_id   = "wizS3Origin"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "wizS3Origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name        = "WizCloudFront"
+    Environment = "Dev"
+  }
+}
